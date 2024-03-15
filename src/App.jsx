2ae98301b373f78c5994fa201ce7b46c5e117a4d
damjan <joveski.damjan@uklo.edu.mk>
2024-03-15 17:16:53 +0100

    Initial commit: Added TailwindCSS Cleaned up boilerplate Improved folder structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return(
+        <div className='w-full mt-10'>
+            <h1 className='text-3xl text-blue-600 w-80 mx-auto'>Hello fellow interns</h1>
+        </div>
+    )
+}
+
+export default App
