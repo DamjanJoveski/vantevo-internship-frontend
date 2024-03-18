1ab1c27ec463569e1d4713da1772fb0031d0eefe
damjan <joveski.damjan@uklo.edu.mk>
2024-03-18 13:35:20 +0100

    Added Listings page with functional filters and company listing preview component. Will improve by defining the filters as separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import {Login} from "./pages/Login/Login.jsx";
-import {Register} from "./pages/Register/Register.jsx";
+import {Listings} from "./pages/Listings/Listings.jsx";
 
 function App() {
 
     return(
         <div className='w-full mt-10'>
-            <h1 className='text-3xl text-blue-600 w-80 mx-auto'>Hello fellow interns</h1>
-            <Login />
-            <Register />
+            <Listings/>
         </div>
     )
 }
