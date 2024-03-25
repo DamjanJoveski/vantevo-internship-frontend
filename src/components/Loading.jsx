0f08efcc2215b9f3104e40373877971682c4fc6c
damjan <joveski.damjan@uklo.edu.mk>
2024-03-25 23:51:35 +0100

    login loading animations done

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,11 @@
 // eslint-disable-next-line react/prop-types
-export function Loading({message= 'Loading'}) {
+export function Loading ({ message }){
     return (
-        <h1>
-            {message}
-        </h1>
-    )
-}
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
+            <div className="bg-white border py-2 px-5 rounded-lg flex items-center flex-col">
+                <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-16 w-16 mb-4"></div>
+                <div className="text-sm">{message}</div>
+            </div>
+        </div>
+    );
+};
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "../../redux/User/UserActions.js";
 import {useDispatch, useSelector} from "react-redux";
+import {Loading} from "../../components/Loading.jsx";
 
 export function Login() {
 
@@ -22,90 +23,103 @@ export function Login() {
     data.append('u_email', user['email'])
     data.append('u_hashedpassword', user['password']);
     dispatch(login(data));
+    redirectTo('/listings')
   };
 
   const userInfo = useSelector(state => state?.user)
 
-  console.log(userInfo.messages.loginStatus) // TREBA DA SE ZAVRSHI
+  let userMessage = null
+  let loginStatus = null
+
+  if(userInfo !== undefined){
+     userMessage= userInfo.messages.message
+     loginStatus = userInfo.messages.loginStatus
+  }
 
   return (
-    <section>
-      <div className="flex flex-col items-center justify-center mx-auto md:h-screen ">
-        <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold text-gray-900 md:text-2xl text-white">
-              Log in to your account
-            </h1>
-            <form
-              className="space-y-4 md:space-y-6"
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleLogin();
-              }}
-            >
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 text-white"
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  value={user.email}
-                  onChange={(e) => setUser({...user, email: e.target.value})}
-                  className="border border-gray-300 sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
-                  placeholder="name@company.com"
-                  required
-                ></input>
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  value={user.password}
-                  onChange={(e) => setUser({...user, password:e.target.value})}
-                  placeholder="••••••••"
-                  className="border border-gray-300 sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
-                  required
-                ></input>
-              </div>
-              <button
-                type="submit"
-                className="w-full font-bold text-white bg-gray-500 p-3 rounded-lg hover:bg-gray-600"
+      <section>
+        <div className="flex flex-col items-center justify-center mx-auto md:h-screen ">
+          <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold text-gray-900 md:text-2xl text-white">
+                Log in to your account
+              </h1>
+              <form
+                  className="space-y-4 md:space-y-6"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    handleLogin();
+                  }}
               >
-                Log in
-              </button>
-              <p className="flex justify-center text-white">
-                Don&apos;t have an account?&nbsp;
+                <div>
+                  <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      value={user.email}
+                      onChange={(e) => setUser({...user, email: e.target.value})}
+                      className="border border-gray-300 sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
+                      placeholder="name@company.com"
+                      required
+                  ></input>
+                </div>
+                <div>
+                  <label
+                      htmlFor="password"
+                      className="block mb-2 text-sm font-medium text-gray-900 text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      value={user.password}
+                      onChange={(e) => setUser({...user, password:e.target.value})}
+                      placeholder="••••••••"
+                      className="border border-gray-300 sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
+                      required
+                  ></input>
+                </div>
                 <button
-                  className="font-bold hover:underline"
-                  onClick={() => redirectTo("/register")}
+                    type="submit"
+                    className="w-full font-bold text-white bg-gray-500 p-3 rounded-lg hover:bg-gray-600"
                 >
-                  Register
+                  Log in
                 </button>
-              </p>
-              <p className="flex justify-center text-white">
-                <button
-                  className="font-bold hover:underline"
-                  onClick={() => redirectTo("/forgot-password")}
-                >
-                  Forgot password?
-                </button>
-              </p>
-            </form>
+                {loginStatus && loginStatus !== 'Success' && loginStatus !== 'Fail' && (
+                    <Loading message={userMessage} />
+                )}
+                {loginStatus === 'Fail' && (
+                 <p className='flex justify-center text-red-600 text-lg'>Login failed! Please try again.</p>
+                )}
+                <p className="flex justify-center text-white">
+                  Don&apos;t have an account?&nbsp;
+                  <button
+                      className="font-bold hover:underline"
+                      onClick={() => redirectTo("/register")}
+                  >
+                    Register
+                  </button>
+                </p>
+                <p className="flex justify-center text-white">
+                  <button
+                      className="font-bold hover:underline"
+                      onClick={() => redirectTo("/forgot-password")}
+                  >
+                    Forgot password?
+                  </button>
+                </p>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
   );
 }
